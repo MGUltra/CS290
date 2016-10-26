7206698a7343ef8bb07a2c51708033e623b7edac
Matt Garner <matthewhgarner@gmail.com>
2016-10-26 18:46:02 -0400

    Assignment complete

--- Assignment3/automobile.js
+++ Assignment3/automobile.js
@@ -4,9 +4,9 @@
 *	Class: CS290 - Web Development
 * HW Assignment: Higher-Order Functions and Objects
 * Description: This program sorts the objects of the Automobile
-* class by their year, make, and type. This is accomplished with
-* one sort function that takes a camparator function along with
-*	the array of objects as parameters.
+* class by their year, make, and type. That task is accomplished with
+* a sort function which takes an array of objects and a comparator 
+* function as parameters.
 ****************************************************************/
 
 
@@ -16,6 +16,20 @@ function Automobile( year, make, model, type ){
     this.make = make; //string (ex. Honda, Ford)
     this.model = model; //string (ex. Accord, Focus)
     this.type = type; //string (ex. Pickup, SUV)
+		
+		
+		Automobile.prototype.logMe = function(typeBool)
+		{
+			if(typeBool == true)
+			{
+				console.log(this.year + " " + this.make + " " + this.model + " " + this.type);
+			}
+			else
+			{
+				console.log(this.year + " " + this.make + " " + this.model);
+			}
+		}
+		
 }
 
 var automobiles = [ 
@@ -29,7 +43,29 @@ var automobiles = [
 
 /*This function sorts arrays using an arbitrary comparator. You pass it a comparator and an array of objects appropriate for that comparator and it will return a new array which is sorted with the largest object in index 0 and the smallest in the last index*/
 function sortArr( comparator, array ){
-    /*your code here*/
+	/*your code here*/
+	
+	var swapFlag = false;
+	
+	do{	
+	
+		swapFlag = false;
+		
+		for (var i = 0; i < array.length - 1; i++)
+		{
+			if(comparator(array[i], array[i+1]) == false) 
+			{
+				var tempAuto = array[i];
+				
+				array[i] = array[i + 1];
+				
+				array[i + 1] = tempAuto;
+				
+				swapFlag = true;
+			}
+		}
+	}while(swapFlag == true);
+  
 }
 
 /*A comparator takes two arguments and uses some algorithm to compare them. If the first argument is larger or greater than the 2nd it returns true, otherwise it returns false. Here is an example that works on integers*/
@@ -46,16 +82,82 @@ function exComparator( int1, int2){
 /*This compares two automobiles based on their year. Newer cars are "greater" than older cars.*/
 function yearComparator( auto1, auto2){
     /* your code here*/
+		
+		if(auto1.year > auto2.year)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 }
 
 /*This compares two automobiles based on their make. It should be case insensitive and makes which are alphabetically earlier in the alphabet are "greater" than ones that come later.*/
 function makeComparator( auto1, auto2){
     /* your code here*/
+		
+		if(auto1.make.toLowerCase() > auto2.make.toLowerCase())
+		{
+			return false;
+		}
+		else 
+		{
+			return true;
+		}
 }
 
 /*This compares two automobiles based on their type. The ordering from "greatest" to "least" is as follows: roadster, pickup, suv, wagon, (types not otherwise listed). It should be case insensitive. If two cars are of equal type then the newest one by model year should be considered "greater".*/
 function typeComparator( auto1, auto2){
     /* your code here*/
+		
+		// define function to get type of Automobile
+		function getType(automobileType)
+		{
+			var typeFlag = 5;
+			
+			if(automobileType == "wagon")
+			{
+				typeFlag = 4;
+			}
+			else if(automobileType == "suv")
+			{
+				typeFlag = 3;
+			}
+			else if(automobileType == "pickup")
+			{
+				typeFlag = 2;
+			}
+			else if(automobileType == "roadster")
+			{
+				typeFlag = 1;
+			}
+			
+			return typeFlag;
+		}
+		
+		// Test for matching types, call exComparitor if match
+		
+		if(auto1.type.toLowerCase() == auto2.type.toLowerCase())
+		{
+			return exComparator(auto1.year, auto2.year);
+		}
+		
+		// Compare types of auto1 and auto2
+		
+		var type1 = getType(auto1.type.toLowerCase());
+		var type2 = getType(auto2.type.toLowerCase());
+		
+		if(type1 > type2)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+		
+		
 }
 
 /*Your program should output the following to the console.log, including the opening and closing 5 stars. All values in parenthesis should be replaced with appropriate values. Each line is a seperate call to console.log.
@@ -81,3 +183,51 @@ The cars sorted by type are:
 
 As an example of the content in the parenthesis:
 1990 Ford F-150 */
+
+
+
+function printArray(sortedArray, typeBool)
+{
+	for(var i = 0; i < sortedArray.length; i++)
+	{
+		sortedArray[i].logMe(typeBool);
+	}
+}
+
+
+
+console.log('*****');
+
+
+
+sortArr(yearComparator, automobiles);
+
+console.log('The cars sorted by year are:');
+
+printArray(automobiles, false);
+
+
+console.log('');
+
+
+sortArr(makeComparator, automobiles);
+
+console.log('The cars sorted by make are:');
+
+printArray(automobiles, false);
+
+
+console.log('');
+
+
+sortArr(typeComparator, automobiles);
+
+console.log('The cars sorted by type are:');
+
+printArray(automobiles, true);
+
+
+console.log('*****');
+
+
+
