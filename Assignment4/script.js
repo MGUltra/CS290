9c9f4952c5375ff7698f7d58cb666ea047db6a5f
Matt Garner <matthewhgarner@gmail.com>
2016-10-30 12:23:01 -0400

    Update script.js

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -1,7 +1,7 @@
 /***************************************************************
 * Name: Matt Garner
-*	Date: 10/30/2016
-*	Class: CS290 - Web Development
+* Date: 10/30/2016
+* Class: CS290 - Web Development
 * HW Assignment: DOM and Events
 * Description: 
 ****************************************************************/
